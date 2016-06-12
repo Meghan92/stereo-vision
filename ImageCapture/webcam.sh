4c8877ba69e66c9ff0cffb93f9a6652020ed1ee9
Meghan92 <megi.vrooyen@gmail.com>
2016-06-12 10:26:01 +0000

    create output folder runtime

--- ImageCapture/webcam.sh
+++ ImageCapture/webcam.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-cd output
-lsof +D /dev
-cd ..
+mkdir output
 LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libv4l/v4l1compat.so fswebcam -r 544x288 -d /dev/video0 /home/pi/Development/stereo-vision/ImageCapture/output/left.jpg &
 LD_PRELOAD=/usr/lib/arm-linux-gnueabihf/libv4l/v4l1compat.so fswebcam -r 544x288 -d /dev/video1 /home/pi/Development/stereo-vision/ImageCapture/output/right.jpg &
 wait
 cd ../FaceDetection
+mkdir output
 python opencv.py
