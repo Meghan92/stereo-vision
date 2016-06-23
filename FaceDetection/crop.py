ea6ed13d12dda9fa207e9e7c1c31ee7186944b13
Meghan92 <megi.vrooyen@gmail.com>
2016-06-23 20:06:50 +0200

    support for multiple captured faces in one image

--- FaceDetection/crop.py
+++ FaceDetection/crop.py
@@ -1,30 +1,33 @@
 import cv2
 
+project_path = "C:/Development/stereo-vision/"
 haar_location = "C:/Program Files/OpenCV/sources/data/haarcascades/"
-raw_capture = "../ImageCapture/"
-left_output = "output/left.jpg"
-right_output = "output/right.jpg"
+raw_capture = project_path + "ImageCapture/"
+left_output = project_path + "FaceDetection/output/left"
+right_output = project_path + "FaceDetection/output/right"
 frontal_face = "haarcascade_frontalface_default.xml"
 
 face_cascade = cv2.CascadeClassifier(haar_location + frontal_face)
-img_left = cv2.imread(raw_capture + left_output)
-img_right = cv2.imread(raw_capture + right_output)
-gray_left = cv2.cvtColor(img_left, cv2.COLOR_BGR2GRAY)
-gray_right = cv2.cvtColor(img_left, cv2.COLOR_BGR2GRAY)
+img_left = cv2.imread(raw_capture + "output/left.jpg")
+img_right = cv2.imread(raw_capture + "output/right.jpg")
 
-left_faces = face_cascade.detectMultiScale(gray_left, 1.3, 5)
+left_faces = face_cascade.detectMultiScale(img_left, 1.3, 5)
+
+i = 0
 
 for (x, y, w, h) in left_faces:
+    ++i
     crop_img = img_left[y:y+h, x:x+w]
-    cv2.imwrite(left_output, crop_img)
+    cv2.imwrite(left_output + "_" + str(i) + ".jpg", crop_img)
 
-right_faces = face_cascade.detectMultiScale(gray_right, 1.3, 5)
+right_faces = face_cascade.detectMultiScale(img_right, 1.3, 5)
+
+i = 0
 
 for (x, y, w, h) in right_faces:
+    ++i
     crop_img = img_right[y:y+h, x:x+w]
-    cv2.imwrite(right_output, crop_img)
-
-
+    cv2.imwrite(right_output + "_" + str(i) + ".jpg", crop_img)
 
 print("Left: ")
 print(left_faces)
