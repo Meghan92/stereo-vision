24b75b1b30a55bb610d40b6767babe6bb6d7978d
Meghan92 <megi.vrooyen@gmail.com>
2016-06-26 17:57:49 +0200

    output gray image

--- preprocessing/convolution/verification.py
+++ preprocessing/convolution/verification.py
@@ -1,2 +1,14 @@
+import cv2
+import os
+
+
 def run():
-    print("verification")
+    path = os.path.dirname(os.path.realpath(__file__))
+    for filename in os.listdir("detection/output"):
+        location = os.path.join(os.path.join(path, "output"), filename)
+        convolve(location)
+
+
+def convolve(location):
+    gray = cv2.cvtColor(cv2.imread(location), cv2.COLOR_RGB)
+    cv2.imwrite("convolution/output/test_gray.jpg", gray)
