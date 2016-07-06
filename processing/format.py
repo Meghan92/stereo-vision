9fa239226bcde6d69d5424b4f76f34cc7b14fc72
Meghan92 <megi.vrooyen@gmail.com>
2016-07-06 20:04:40 +0200

    format step that takes the bytes and converts it to a neat output format and returns the input array for the neural network

---
+++ processing/format.py
@@ -0,0 +1,24 @@
+def run():
+    number_map = []
+    input_array = []
+    with open("output/bytes.txt", "r") as text:
+        for line in text:
+            data_val = line.split(";")
+            number = data_val[0].split("_")[1]
+            array = map(int, line.split("[")[1].split("]")[0].split(","))
+            if number_map.__len__() > 0 and number_map.__contains__(number):
+                index = number_map.index(number)
+                input_array[index] += array
+            else:
+                input_array.append(array)
+                number_map.append(number)
+    text.close()
+    text = open("output/inputs.txt", "w")
+    for array in input_array:
+        text.write(str(array))
+    text.close()
+    return input_array
+
+
+
+
