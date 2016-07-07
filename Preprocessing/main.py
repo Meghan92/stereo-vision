4f1687ff08bfde41a5b87aa99d93810e18ef0c18
Meghan92 <megi.vrooyen@gmail.com>
2016-07-07 20:13:35 +0200

    made detect reusable

--- Preprocessing/main.py
+++ Preprocessing/main.py
@@ -1,11 +1,15 @@
 import preprocessing.detection.crop as crop
 import preprocessing.detection.detect as detect
 import preprocessing.detection.strategy as strategy
+import preprocessing.detection.config as detect_config
 import clean
 
 try:
     print("-----Detecting faces-----")
-    detect.run()
+    environment = detect_config.Environment(detect_config.Windows("output/left.jpg", "left.jpg"))
+    detect.run(environment)
+    environment = detect_config.Environment(detect_config.Windows("output/right.jpg", "right.jpg"))
+    detect.run(environment)
     print("-----Cropping faces-----")
     crop.run(16)
     print("-----Running strategy-----")
--- preprocessing/detection/detect.py
+++ preprocessing/detection/detect.py
@@ -1,41 +1,12 @@
 import cv2
 
 
-def run():
-    project_path = "C:/Development/stereo-vision/"
-    haar_location = "C:/Program Files/OpenCV/sources/data/haarcascades/"
-    raw_capture = project_path + "capture/"
-    left_output = project_path + "preprocessing/detection/output/left"
-    right_output = project_path + "preprocessing/detection/output/right"
-    frontal_face = "haarcascade_frontalface_default.xml"
-
-    face_cascade = cv2.CascadeClassifier(haar_location + frontal_face)
-    img_left = cv2.imread(raw_capture + "output/left.jpg")
-    img_right = cv2.imread(raw_capture + "output/right.jpg")
-
-    left_faces = face_cascade.detectMultiScale(img_left, 1.3, 5)
-
+def run(environment):
+    face_cascade = cv2.CascadeClassifier(environment.haar_xml)
+    img = cv2.imread(environment.capture)
+    faces = face_cascade.detectMultiScale(img, 1.3, 5)
     i = 0
-
-    for (x, y, w, h) in left_faces:
+    for (x, y, w, h) in faces:
         i += 1
-        crop_img = img_left[y:y+h, x:x+w]
-        cv2.imwrite(left_output + "_" + str(i) + ".jpg", crop_img)
-
-    right_faces = face_cascade.detectMultiScale(img_right, 1.3, 5)
-
-    i = 0
-
-    for (x, y, w, h) in right_faces:
-        i += 1
-        crop_img = img_right[y:y+h, x:x+w]
-        cv2.imwrite(right_output + "_" + str(i) + ".jpg", crop_img)
-
-    print("Left: ")
-    print(left_faces)
-
-    print("Right: ")
-    print(right_faces)
-
-
-
+        crop_img = img[y:y+h, x:x+w]
+        cv2.imwrite(environment.output, crop_img)
