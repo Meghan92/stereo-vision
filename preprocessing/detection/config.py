93eb91e90e2ccbf737fe089e967565a2bb80e575
meghan92 <megi.vrooyen@gmail.com>
2016-07-09 12:40:54 +0200

    added ubuntu configuration

--- preprocessing/detection/config.py
+++ preprocessing/detection/config.py
@@ -13,3 +13,11 @@ class Windows(object):
         self.path = "C:/Development/stereo-vision/"
         self.input = input_location
         self.output = output_name
+
+
+class Ubuntu(object):
+	def __init__(self, input_location, output_name):
+		self.haar = "/usr/share/opencv/haarcascades/"
+		self.path = "/home/meghan/Development/stereo-vision/"
+		self.input = input_location
+		self.output = output_name
