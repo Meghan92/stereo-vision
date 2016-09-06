ec765adda8f91a9cefb2024c29514253f390d99c
meghan92 <megi.vrooyen@gmail.com>
2016-09-06 19:59:57 +0200

    Added various TODOs

--- scripts/business/student.py
+++ scripts/business/student.py
@@ -2,5 +2,6 @@ import scripts.data.model as model
 import scripts.data.repository.student as repository
 
 def create(student_id, first_name, last_name):
+	#TODO: make sure user does not already exist and return true if successful
 	student = model.Student(student_id, first_name, last_name)	
 	repository.insert(student)
--- scripts/register.py
+++ scripts/register.py
@@ -8,6 +8,7 @@ def start():
 	first_name = raw_input("Please enter your first name: ")
 	last_name = raw_input("Please enter your last name: ")
 	response = business.student.create(student_id, first_name, last_name)
+	#TODO: capture face if successful
 	
 	
 	
