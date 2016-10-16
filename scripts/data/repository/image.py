3c7a685e1a8e8b6a4886047565f9436eb80a23ff
meghan92 <megi.vrooyen@gmail.com>
2016-10-16 20:10:20 +0200

    Added an image get and an image description get to image repository

--- scripts/data/repository/image.py
+++ scripts/data/repository/image.py
@@ -1,15 +1,39 @@
 import MySQLdb
 import config
+import time
 
 
 def insert(image):
 	connection = config.ConnectionString()
 	db = connection.database;
 	cursor = db.cursor()
-	query = """INSERT INTO Image VALUES ({sid}, '{imt}', '{li}', '{ri}')""".\
-		format(sid=image.student_id, imt=image.image_type, li=image.left_image, ri=image.right_image)
+	now = time.strftime('%Y-%m-%d %H:%M:%S')
+	query = """INSERT INTO Image VALUES ({sid}, '{imt}', '{li}', '{ri}', '{cdt}', '{udt}')""".\
+		format(sid=image.student_id, imt=image.image_type, li=image.left_image, ri=image.right_image, cdt=now, udt='NULL')
 	cursor.execute(query)
 	db.commit()
 	db.close()
 	
 
+def get(student_id, image_type_id):
+	connection = config.ConnectionString()
+	db = connection.database;
+	cursor = db.cursor()
+	query = """SELECT * FROM Image WHERE student_id = '{sid}' AND image_type_id = '{imt}'""".\
+		format(sid=student_id, imt=image_type_id)
+	cursor.execute(query)
+	db.commit()
+	db.close()
+	return cursor.fetchone()
+	
+	
+def get_type(image_type_id):
+	connection = config.ConnectionString()
+	db = connection.database;
+	cursor = db.cursor()
+	query = """SELECT * FROM ImageType WHERE image_type_id = '{imt}'""".\
+		format(sid=student_id, imt=image_type_id)
+	cursor.execute(query)
+	db.commit()
+	db.close()
+	return cursor.fetchone()
