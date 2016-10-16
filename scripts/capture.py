c42bb759092ee82cb216959966a17c63be41d4b2
meghan92 <megi.vrooyen@gmail.com>
2016-10-16 20:08:54 +0200

    Added a blob save method on capture, to be used for registration

--- scripts/capture.py
+++ scripts/capture.py
@@ -1,6 +1,7 @@
 import webcam
 import display
 import common.exceptions
+import business.image as images
 
 #capture.py is used for recording images
 #if an image has not been captured,
@@ -30,6 +31,11 @@ def frontal():
 				print("\nInvalid input (" + try_again + ") Please either specify \"y\" or \"n\"")
 				
 
-def save(image_type):
-	blob_array = webcam.get_blobs()	
+def save(student_id, image_type_id):
+	blob_array = webcam.get_blobs()
+	array_length = blob_array.__length__()
+	if array_length == 2:
+		images.create(student_id, image_type_id, blob_array)
+	else:
+		raise ValueError("Two images are required for storing to the database. Current length: {}".format(array_length))	
 			
