3c44bbff3f47b88d9fe5b991d3f421010ae895b5
meghan92 <megi.vrooyen@gmail.com>
2016-10-17 18:20:13 +0200

    Added InvalidLogin exception model

--- scripts/common/exceptions.py
+++ scripts/common/exceptions.py
@@ -2,4 +2,11 @@ class CaptureError():
 
 	def __init__(self):
 		self.message = "Your image could not be captured. Please view the error log for details"
+		
+		
+class InvalidLogin():
+
+	def __init__(self, _message, _student_number):
+		self.message = _message;
+		self.student_number = _student_number;
 
