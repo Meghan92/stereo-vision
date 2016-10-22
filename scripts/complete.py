a44d950d91f403da70bcd2de735b7bce76a56473
meghan92 <megi.vrooyen@gmail.com>
2016-10-22 15:20:31 +0200

    Added student number to complete function for welcome or invalid login display

--- scripts/complete.py
+++ scripts/complete.py
@@ -1,12 +1,13 @@
 import webbrowser
 
 
-def success():
+def success(student_number):
 	webbrowser.open_new("https://img.buzzfeed.com/buzzfeed-static/static/2015-04/14/11/enhanced/webdr13/enhanced-mid-13335-1429024178-2.jpg")
-	raw_input("Welcome! Opening Web Browser. Hit enter to close the application")
+	raw_input("Welcome {} ! Opening Web Browser. Hit enter to close the application".format(student_number))
 	
 
-def fail():
+def fail(student_number):
+	#TODO: audit this login failure
 	webbrowser.open_new("https://socialmediaweek.org/wp-content/blogs.dir/1/files/social-fail-940x492.jpg")
-	raw_input("Invalid! Opening Web Browser. Hit enter to close the application")
+	raw_input("Invalid login for {}. Opening Web Browser. Hit enter to close the application".format(student_number))
 	
