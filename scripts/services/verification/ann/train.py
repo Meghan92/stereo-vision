bab99b3b197538d8717780d049b37321b24aaeeb
meghan92 <megi.vrooyen@gmail.com>
2016-11-11 21:58:35 +0200

    Added classification report

--- scripts/services/verification/ann/train.py
+++ scripts/services/verification/ann/train.py
@@ -1,5 +1,6 @@
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import classification_report
+from sklearn.metrics import classification_report, accuracy_score
 from sklearn import datasets
 from nolearn.dbn import DBN
 import numpy as np
@@ -8,18 +9,23 @@ import cPickle as pickle
 import os
 
 
+
 def run(_input, _output):
+
+	x_train, x_test, y_train, y_test = 	train_test_split(_input, _output, test_size=0.33, random_state=42)
 	dbn = DBN(
-		[64, 32, 2],
+		[64, 200, 2],
 		learn_rates = 0.3,
 		learn_rate_decays = 0.9,
-		epochs = 20,
+		epochs = 100,
 		verbose = 1,
 		minibatch_size=1)
-	dbn.fit(_input, _output)
+	dbn.fit(x_train, y_train)
 	current_path = os.path.dirname(__file__)
 	weights_path = os.path.join(current_path, "weights.pkl")
 	save_object(dbn, weights_path)
+	preds = dbn.predict(x_test)
+	print classification_report(y_test, preds)
 
 
 def save_object(obj, filename):
