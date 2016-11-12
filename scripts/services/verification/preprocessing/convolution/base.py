cf96d31792cb574fdfd9824e566b2eb633e3f571
meghan92 <megi.vrooyen@gmail.com>
2016-11-12 12:21:58 +0200

    Fixed the sobel operator

--- scripts/services/verification/preprocessing/convolution/base.py
+++ scripts/services/verification/preprocessing/convolution/base.py
@@ -3,8 +3,11 @@ import numpy as np
 
 
 def convolve(image, location):
-	sobel_converted = cv2.Sobel(image, cv2.CV_16S, 1, 0, ksize=3)
-	absolute_sobel = np.absolute(sobel_converted)
-	uint8_sobel = np.uint8(absolute_sobel)
+	blur = cv2.GaussianBlur(image, ksize=(3,3), sigmaX=0, sigmaY=0)
+	sobel_x = cv2.Sobel(blur, cv2.CV_16S, 1, 0, ksize=3)
+	sobel_y = cv2.Sobel(blur, cv2.CV_16S, 0, 1, ksize=3)
+	abs_sobel_x = cv2.convertScaleAbs(sobel_x)
+	abs_sobel_y = cv2.convertScaleAbs(sobel_y)
+	weighted = cv2.addWeighted(abs_sobel_x, 0.5, abs_sobel_y, 0.5, gamma=0)
 	print "- Writing to " + location
-	cv2.imwrite(location, uint8_sobel)
+	cv2.imwrite(location, weighted)
