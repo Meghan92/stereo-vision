b58fc6d6f08120c0ecf0bffd58e0d98ca3fadc84
Meghan92 <megi.vrooyen@gmail.com>
2016-11-16 20:06:51 +0200

    Updated ann main reading in

--- scripts/services/verification/ann/main.py
+++ scripts/services/verification/ann/main.py
@@ -1,37 +1,35 @@
-import sys
-import os.path
-sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-import processing.format as format
-import ann.train as train
+import scripts.services.verification.ann.train as train
 import numpy as np
-import preprocessing.convolution.output as output
+import scripts.services.verification.preprocessing.convolution.output as output
 
 
 def run(resolution):
-	images = output.get(resolution)
-	image_map = {}
-	for image in images:
-		id = image.name.split("_")[0] + image.name.split("_")[1]
-		if id in image_map:
-			image_map[id] += image.image
-		else:
-			image_map[id] = image.image
-	input_array = []
-	output_array = []
-	for key, image in image_map.iteritems():
-		input_array.append(flatten_rgb(image))
-		if "spoof" in key:
-			output_array.append(-1)
-		else:
-			output_array.append(1)
-		
-	input_array = np.array(input_array)
-	output_array = np.array(output_array)
+    images = output.get(resolution)
+    image_map = {}
+    for image in images:
+        image_id = image.name.split("_")[0].replace("left", "").replace("right", "") + image.name.split("_")[1]
+        image_pair = [None] * 2
+        if image_id in image_map:
+            image_pair[1] = image.image
+        else:
+            image_pair[0] = image.image
+        image_map[image_id] = image_pair
+    input_array1 = []
+    input_array2 = []
+    output_array = []
+    for key, image_pair in image_map.iteritems():
+        input_array1.append(image_pair[0])
+        input_array2.append(image_pair[1])
+        if "spoof" in key:
+            output_array.append(-1)
+        else:
+            output_array.append(1)
 
-	train.run(input_array, output_array)
+    input_array = np.array([input_array1, input_array1])
+    output_array = np.array(output_array)
+
+    train.run(input_array, output_array)
 
 
 def flatten_rgb(image):
-	return image.flatten()/255.0
-		
-		
+    return image.flatten() / 255.0
